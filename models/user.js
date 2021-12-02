ce37b9fc9c51d274c9adaa50b1883299fd093c83
Markos <markosmk@me.com>
2021-12-02 15:27:13 -0300

    [update] union customers and users model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
@@ -11,26 +12,87 @@ const userSchema = mongoose.Schema(
       type: String,
       required: false,
     },
+    /*
+    AN: { // numero cuenta
+      type: String,
+      required: true,
+      default: 'asdaf'
+    },
+*/
+    // datos authentica
     email: {
       type: String,
       required: true,
-      index: { unique: true },
+      trim: true, // recorta los espacios vacios a ambos lados
+      lowercase: true, // normaliza el email a todo minusculas
+      index: { unique: true }, // el email debe ser unico
     },
     password: {
       type: String,
       required: true,
+      minlength: 8, // La contraseña necesita al menos 8 caracteres
+      trim: true,
     },
     avatar: {
       type: String,
       required: false,
     },
+    // datos ubicacion
+    address: {
+      type: String,
+      required: false,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    dni: {
+      type: String,
+      required: false,
+    },
+    department: {
+      type: String,
+      required: false,
+    },
+    // datos de servicio
+    location: {
+      type: String,
+      required: false,
+      enum: {
+        values: ['Tunuyan', 'San-Rafael', 'Malargue'],
+        message: '{VALUE} no esta permitido',
+      },
+    },
+    service: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Service',
+      required: false,
+      index: true,
+      // relacionado 1 to 1
+    },
+    // datos para admin/cuenta
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
     logged_in: {
       type: Boolean,
       default: false,
     },
+    last_session: {
+      type: Date,
+      required: false,
+    },
     verified: {
       type: Boolean,
       default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+      default: () => crypto.randomBytes(20).toString('hex'),
     },
   },
   {
