9a293104ecdc499b857692217bb1134be8452dff
Markos <markosmk@me.com>
2021-12-28 23:11:16 -0300

    [update] add swaggers docs in /docs

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const { errorHandler } = require('./utils/errorHandler');
 const app = express();
 
 // para recibir data en json
 app.use(express.json());
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'CTC REST API',
+      description:
+        'A REST API built with Express and MongoDB. This API provides services and the context of the services for customers.',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 // rutas
 const serviceRoutes = require('./routes/service');
@@ -12,7 +26,9 @@ const userRoutes = require('./routes/user');
 // base rutas
 app.use('/api', serviceRoutes);
 app.use('/api', userRoutes);
-
+// documentacion de ApiRest con Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// otras Rutas
 app.get('*', function (req, res, next) {
   const error = new Error(`${req.ip} tried to access ${req.originalUrl}`);
   error.statusCode = 404;
