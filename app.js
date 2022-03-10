9a2f363dda117abf33d745624cba75f5ee55ca22
Markos <markosmk@me.com>
2022-03-10 13:00:17 -0300

    update: without swagger mommently

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const { errorHandler } = require('./utils/errorHandler');
+const { errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 
+// Middlewares
 // para parsear data en json
 app.use(express.json());
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'CTC REST API',
-      description:
-        'A REST API built with Express and MongoDB. This API provides services and the context of the services for customers.',
-    },
-  },
-  apis: ['./routes/*.js'],
-};
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api/v1', require('./routes'));
 
-// rutas
-const serviceRoutes = require('./routes/service');
-const userRoutes = require('./routes/user');
-
-// base rutas
-app.use('/api', serviceRoutes);
-app.use('/api', userRoutes);
-// documentacion de ApiRest con Swagger
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // resolucion rutas no asignadas
 app.get('*', function (req, res, next) {
   const error = new Error(`${req.ip} tried to access ${req.originalUrl}`);
