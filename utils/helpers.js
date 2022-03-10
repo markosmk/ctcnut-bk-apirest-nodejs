456894d9974f596aa105524e2f4f066cd7e278e8
Markos <markosmk@me.com>
2022-03-10 12:54:26 -0300

    moved: change of folder

--- utils/helpers.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports.validID = (req, _, next) => {
-  if (!mongoose.isValidObjectId(req.params.id)) {
-    return next(new Error('Identificador Invalido'));
-  }
-  next();
-};
